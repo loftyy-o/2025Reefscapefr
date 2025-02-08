98e6ce516642c31d74953f961886956968c60832
loftyy-o <chimao.okafor123@gmail.com>
2025-02-08 14:59:52 -0800

    sooooo
    the fix didnt work the first

--- robot_container.py
+++ robot_container.py
@@ -1,6 +1,6 @@
 import commands2
 import commands2.button
-import commands2.cmd
+from commands2 import cmd
 from commands2.sysid import SysIdRoutine
 from pathplannerlib.auto import AutoBuilder
 from pathplannerlib.path import PathConstraints, PathPlannerPath
@@ -11,11 +11,11 @@ from wpimath.units import rotationsToRadians
 
 from generated.tuner_constants import TunerConstants
 from robot_state import RobotState
-from subsystems.superstructure import Superstructure
-
 from subsystems.climber import ClimberSubsystem
-from subsystems.pivot import Pivot
-from subsystems.intake import Intake
+from subsystems.elevator import ElevatorSubsystem
+from subsystems.intake import IntakeSubsystem
+from subsystems.pivot import PivotSubsystem
+from subsystems.superstructure import Superstructure
 
 
 class RobotContainer:
@@ -36,14 +36,30 @@ class RobotContainer:
         self.drivetrain = TunerConstants.create_drivetrain()
 
         self.climber = ClimberSubsystem()
-        self.superstructure = Superstructure(self.drivetrain)
+        self.pivot = PivotSubsystem()
+        self.intake = IntakeSubsystem()
+        self.elevator = ElevatorSubsystem()
 
-        self.pivot = Pivot()
-        self.intake = Intake()
+        self.superstructure = Superstructure(self.drivetrain, self.pivot, self.elevator)
+        self.robot_state = RobotState(self.drivetrain, self.pivot, self.elevator)
 
-        self.superstructure = Superstructure(self.drivetrain, self.pivot)
-
-        self._robot_state = RobotState(self.drivetrain)
+        # These are the paths that the robot can follow, which are preloaded so we reference them later and reduce lag.
+        self.preloaded_paths = {
+            "Coral A" : PathPlannerPath.fromPathFile("Coral A"),
+            "Coral B" : PathPlannerPath.fromPathFile("Coral B"),
+            "Coral C" : PathPlannerPath.fromPathFile("Coral C"),
+            "Coral D" : PathPlannerPath.fromPathFile("Coral D"),
+            "Coral E" : PathPlannerPath.fromPathFile("Coral E"),
+            "Coral F" : PathPlannerPath.fromPathFile("Coral F"),
+            "Coral G" : PathPlannerPath.fromPathFile("Coral G"),
+            "Coral H" : PathPlannerPath.fromPathFile("Coral H"),
+            "Coral I" : PathPlannerPath.fromPathFile("Coral I"),
+            "Coral J" : PathPlannerPath.fromPathFile("Coral J"),
+            "Coral K" : PathPlannerPath.fromPathFile("Coral K"),
+            "Coral L" : PathPlannerPath.fromPathFile("Coral L"),
+            "Coral Station 1" : PathPlannerPath.fromPathFile("Coral Station 1"),
+            "Coral Station 2" : PathPlannerPath.fromPathFile("Coral Station 2"),
+        } # Ends the dictionary
 
         # Setting up bindings for necessary control of the swerve drive platform
         self._field_centric = (
@@ -119,67 +135,56 @@ class RobotContainer:
             self.drivetrain.runOnce(lambda: self.drivetrain.seed_field_centric())
         )
 
-        path_state = "DEFAULT"
-        if self._driver_controller.getLeftTriggerAxis() >= self.trigger_margin and self._driver_controller.getRightTriggerAxis() >= self.trigger_margin:
-            path_state = "CORALSTATION"
-        elif self._driver_controller.getLeftTriggerAxis() >= self.trigger_margin:
-            path_state = "LEFT"
-        elif self._driver_controller.getRightTriggerAxis() >= self.trigger_margin:
-            path_state = "RIGHT"
+        #Left reef sides
+        (commands2.button.Trigger(lambda: self._driver_controller.getLeftTriggerAxis() >= .75) & self._driver_controller.y()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral A"], self.path_constraints)
+                )
+        (commands2.button.Trigger(lambda: self._driver_controller.getLeftTriggerAxis() >= .75) & self._driver_controller.x()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral C"], self.path_constraints)
+                )
+        (commands2.button.Trigger(lambda: self._driver_controller.getLeftTriggerAxis() >= .75) & self._driver_controller.a()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral E"], self.path_constraints)
+                )
+        (commands2.button.Trigger(lambda: self._driver_controller.getLeftTriggerAxis() >= .75) & self._driver_controller.b()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral G"], self.path_constraints)
+                )
+        (commands2.button.Trigger(lambda: self._driver_controller.getLeftTriggerAxis() >= .75) & self._driver_controller.rightBumper()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral I"], self.path_constraints)
+                )
+        (commands2.button.Trigger(lambda: self._driver_controller.getLeftTriggerAxis() >= .75) & self._driver_controller.leftBumper()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral K"], self.path_constraints)
+                )  
 
-
-        match path_state:
-            case "LEFT":
-                self._driver_controller.y().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral A"), self.path_constraints)
+        #right reef sides
+        (commands2.button.Trigger(lambda: self._driver_controller.getRightTriggerAxis() >= .75) & self._driver_controller.y()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral B"], self.path_constraints)
                 )
-                self._driver_controller.x().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral C"), self.path_constraints)
+        (commands2.button.Trigger(lambda: self._driver_controller.getRightTriggerAxis() >= .75) & self._driver_controller.x()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral D"], self.path_constraints)
                 )
-                self._driver_controller.a().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral E"), self.path_constraints)
+        (commands2.button.Trigger(lambda: self._driver_controller.getRightTriggerAxis() >= .75) & self._driver_controller.a()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral F"], self.path_constraints)
                 )
-                self._driver_controller.b().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral G"), self.path_constraints)
+        (commands2.button.Trigger(lambda: self._driver_controller.getRightTriggerAxis() >= .75) & self._driver_controller.b()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral H"], self.path_constraints)
                 )
-                self._driver_controller.rightBumper().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral I"), self.path_constraints)
+        (commands2.button.Trigger(lambda: self._driver_controller.getRightTriggerAxis() >= .75) & self._driver_controller.rightBumper()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral J"], self.path_constraints)
                 )
-                self._driver_controller.leftBumper().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral K"), self.path_constraints)
+        (commands2.button.Trigger(lambda: self._driver_controller.getRightTriggerAxis() >= .75) & self._driver_controller.leftBumper()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral L"], self.path_constraints)
                 )
-            case "RIGHT":
-                self._driver_controller.y().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral B"), self.path_constraints)
-                )
-                self._driver_controller.x().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral D"), self.path_constraints)
-                )
-                self._driver_controller.a().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral F"), self.path_constraints)
-                )
-                self._driver_controller.b().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral H"), self.path_constraints)
-                )
-                self._driver_controller.b().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral J"), self.path_constraints)
-                )
-                self._driver_controller.leftBumper().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral L"), self.path_constraints)
-                )
-            case "CORALSTATION":
-                self._driver_controller.leftBumper().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral Station 1"), self.path_constraints)
-                )
-                self._driver_controller.rightBumper().whileTrue(
-                    AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("Coral Station 2"), self.path_constraints)
-                )
-            case _:
-                pass
-
         
+        #coral stations
+        (commands2.button.Trigger(lambda: self._driver_controller.getRightTriggerAxis() >= .75) & commands2.button.Trigger(lambda: self._driver_controller.getLeftTriggerAxis() >= .75) & self._driver_controller.leftBumper()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral Station 1"], self.path_constraints)
+                )
+        (commands2.button.Trigger(lambda: self._driver_controller.getRightTriggerAxis() >= .75) & commands2.button.Trigger(lambda: self._driver_controller.getLeftTriggerAxis() >= .75) & self._driver_controller.rightBumper()).whileTrue(
+                    AutoBuilder.pathfindThenFollowPath(self.preloaded_paths["Coral Station 2"], self.path_constraints)
+                )
 
-        self._driver_controller.rightBumper().whileTrue(
+        (commands2.button.Trigger(lambda: self._driver_controller.getLeftTriggerAxis() < .75) & commands2.button.Trigger(lambda: self._driver_controller.getRightTriggerAxis() < .75) & self._driver_controller.rightBumper()).whileTrue( #Only does this function if the triggers aren't pressed
+
             self.drivetrain.apply_request(
                 lambda: (
                     self._robot_centric.with_velocity_x(
@@ -194,17 +199,101 @@ class RobotContainer:
                 )
             )
         )
-        self._function_controller.y().whileTrue(
-            self.climber.set_desired_state_command(self.climber.SubsystemState.CLIMB_POSITIVE)).onFalse(
+
+        self._function_controller.y().onTrue(
+            self.superstructure.set_goal_command(self.superstructure.Goal.L4_SCORING)
+        )
+
+        self._function_controller.x().onTrue(
+            self.superstructure.set_goal_command(self.superstructure.Goal.L3_SCORING)
+        )
+
+        self._function_controller.b().onTrue(
+            self.superstructure.set_goal_command(self.superstructure.Goal.L2_SCORING)
+        )
+
+        self._function_controller.a().onTrue(
+            self.superstructure.set_goal_command(self.superstructure.Goal.L1_SCORING)
+        )
+
+        (self._function_controller.y() & self._function_controller.start()).onTrue(
+            self.superstructure.set_goal_command(self.superstructure.Goal.ALGAE_SCORING_NET)
+        )
+
+        (self._function_controller.x() & self._function_controller.start()).onTrue(
+            self.superstructure.set_goal_command(self.superstructure.Goal.L3_ALGAE_INTAKE)
+        )
+
+        (self._function_controller.b() & self._function_controller.start()).onTrue(
+            self.superstructure.set_goal_command(self.superstructure.Goal.L2_ALGAE_INTAKE)
+        )
+
+        (self._function_controller.a() & self._function_controller.start()).onTrue(
+            self.superstructure.set_goal_command(self.superstructure.Goal.ALGAE_SCORING_PROCESSOR)
+        )
+
+        self._function_controller.leftBumper().whileTrue(
+            cmd.parallel(
+                self.superstructure.set_goal_command(self.superstructure.Goal.FUNNEL_INTAKE),
+                self.intake.set_desired_state_command(self.intake.SubsystemState.INTAKING)
+            )
+        )
+
+        (self._function_controller.leftBumper() & self._function_controller.back()).whileTrue(
+            cmd.parallel(
+                self.superstructure.set_goal_command(self.superstructure.Goal.GROUND_INTAKE),
+                self.intake.set_desired_state_command(self.intake.SubsystemState.INTAKING)
+            )
+        )
+
+        self._function_controller.rightBumper().whileTrue(
+            self.intake.set_desired_state_command(self.intake.SubsystemState.OUTPUTTING)
+        )
+
+        (self._function_controller.leftStick() & self._function_controller.rightStick()).whileTrue(
+            self.superstructure.set_goal_command(self.superstructure.Goal.DEFAULT)
+        )
+
+        self._function_controller.povLeft().whileTrue(
+            self.climber.set_desired_state_command(self.climber.SubsystemState.CLIMB_POSITIVE)
+        ).onFalse(
             self.climber.set_desired_state_command(self.climber.SubsystemState.STOP)
         )
-        
-        self._function_controller.x().whileTrue(
-            self.climber.set_desired_state_command(self.climber.SubsystemState.CLIMB_NEGATIVE)).onFalse(
+
+        self._function_controller.povRight().whileTrue(
+            self.climber.set_desired_state_command(self.climber.SubsystemState.CLIMB_NEGATIVE)
+        ).onFalse(
             self.climber.set_desired_state_command(self.climber.SubsystemState.STOP)
         )
+
+        (self._function_controller.povUp() & self._function_controller.y()).onTrue(commands2.InstantCommand(lambda: SignalLogger.start())).whileTrue(
+            self.elevator.sys_id_dynamic(SysIdRoutine.Direction.kForward).onlyIf(lambda: not DriverStation.isFMSAttached())
+        )
+        (self._function_controller.povUp() & self._function_controller.a()).onTrue(commands2.InstantCommand(lambda: SignalLogger.start())).whileTrue(
+            self.elevator.sys_id_dynamic(SysIdRoutine.Direction.kReverse).onlyIf(lambda: not DriverStation.isFMSAttached())
+        )
+        (self._function_controller.povDown() & self._function_controller.y()).onTrue(commands2.InstantCommand(lambda: SignalLogger.start())).whileTrue(
+            self.elevator.sys_id_quasistatic(SysIdRoutine.Direction.kForward).onlyIf(lambda: not DriverStation.isFMSAttached())
+        )
+        (self._function_controller.povDown() & self._function_controller.a()).onTrue(commands2.InstantCommand(lambda: SignalLogger.start())).whileTrue(
+            self.elevator.sys_id_quasistatic(SysIdRoutine.Direction.kReverse).onlyIf(lambda: not DriverStation.isFMSAttached())
+        )
+
+        (self._function_controller.povLeft() & self._function_controller.y()).onTrue(commands2.InstantCommand(lambda: SignalLogger.start())).whileTrue(
+            self.pivot.sys_id_dynamic(SysIdRoutine.Direction.kForward).onlyIf(lambda: not DriverStation.isFMSAttached())
+        )
+        (self._function_controller.povLeft() & self._function_controller.a()).onTrue(commands2.InstantCommand(lambda: SignalLogger.start())).whileTrue(
+            self.pivot.sys_id_dynamic(SysIdRoutine.Direction.kReverse).onlyIf(lambda: not DriverStation.isFMSAttached())
+        )
+        (self._function_controller.povRight() & self._function_controller.y()).onTrue(commands2.InstantCommand(lambda: SignalLogger.start())).whileTrue(
+            self.pivot.sys_id_quasistatic(SysIdRoutine.Direction.kForward).onlyIf(lambda: not DriverStation.isFMSAttached())
+        )
+        (self._function_controller.povRight() & self._function_controller.a()).onTrue(commands2.InstantCommand(lambda: SignalLogger.start())).whileTrue(
+            self.pivot.sys_id_quasistatic(SysIdRoutine.Direction.kReverse).onlyIf(lambda: not DriverStation.isFMSAttached())
+        )
+
         self.drivetrain.register_telemetry(
-            lambda state: self._robot_state.log_swerve_state(state)
+            lambda state: self.robot_state.log_swerve_state(state)
         )
 
     def get_autonomous_command(self) -> commands2.Command:
--- subsystems/superstructure.py
+++ subsystems/superstructure.py
@@ -1,45 +1,149 @@
+"""
+File: swerve.py
+Description: The swerve subsystem, plus method to add MegaTag2 pose estimate.
+Main Author: Caden Dalley
+Co-Authors: Micah Nguyen, James Haddix
+"""
+
 from enum import auto, Enum
 
 from commands2 import Command, Subsystem, cmd
 from wpilib import DriverStation, SmartDashboard
 
 from subsystems.swerve import SwerveSubsystem
-from subsystems.pivot import Pivot
+from subsystems.pivot import PivotSubsystem
+from subsystems.elevator import ElevatorSubsystem
 
 class Superstructure(Subsystem):
-        
+    """
+    The Superstructure is in charge of controlling the elevator and the end effector.
+    """
+
     class Goal(Enum):
         DEFAULT = auto()
+        L4_SCORING = auto()
+        L3_SCORING = auto()
+        L2_SCORING = auto()
+        L1_SCORING = auto()
+        L2_ALGAE_INTAKE = auto()
+        L3_ALGAE_INTAKE = auto()
+        ALGAE_SCORING_PROCESSOR = auto()
+        ALGAE_SCORING_NET = auto()
+        FUNNEL_INTAKE = auto()
+        GROUND_INTAKE = auto()
+        
+    def __init__(self, drivetrain: SwerveSubsystem, pivot: PivotSubsystem, elevator: ElevatorSubsystem) -> None:
+        """
+        Constructs the superstructure using instance of each subsystem.
 
-    def __init__(self, drivetrain: SwerveSubsystem, pivot: Pivot) -> None:
+        :param drivetrain: Swerve drive base
+        :type drivetrain:  SwerveSubsystem
+        :param pivot:      Pivot that rotates our intake
+        :type pivot:       PivotSubsystem
+        :param elevator:   Elevator that moves the intake up and down
+        :type elevator:    ElevatorSubsystem
+        """
+
+        # __init__ from Subsystem class
         super().__init__()
 
+        # create subsystems
         self.drivetrain = drivetrain
         self.pivot = pivot
-        
+        self.elevator = elevator
+
+        # set up goal with the default goal being the current goal as well as the last goal
         self._goal = self.Goal.DEFAULT
         self._last_goal = self.Goal.DEFAULT
-
+    
     def periodic(self):
+        # Do nothing if in test mode
+        if DriverStation.isTest():
+            return
+
+        # if the driver station is disabled, set the default goal with the requirement of the superstructure.
         if DriverStation.isDisabled():
             default_command = self.set_goal_command(self.Goal.DEFAULT)
             default_command.addRequirements(self)
             self.setDefaultCommand(default_command)
 
+        # periodically the goal becomes the last goal (we save what the last goal was)
         self._last_goal = self._goal
+
+        # put the goal's name on SmartDashboard
         SmartDashboard.putString("Superstructure Goal", self._goal.name)
-        
+
+        # match the goal against each possible goal for the superstructure
         match self._goal:
+            
+            # the default goal: the pivot is stowed, the elevator is lowered
             case self.Goal.DEFAULT:
-                self.pivot.set_desired_state(Pivot.SubsystemState.STOW)
-                
-        
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.STOW)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.DEFAULT)
+            
+            # goals for each scoring level on the reef (L1-L4)
+            case self.Goal.L4_SCORING:
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.HIGH_SCORING)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.L4)
+
+            case self.Goal.L3_SCORING:
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.MID_SCORING)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.L3)
+
+            case self.Goal.L2_SCORING:
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.MID_SCORING)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.L2)
+
+            case self.Goal.L1_SCORING:
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.LOW_SCORING)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.L1)
+
+            # goal for intaking through our funnel: the pivot is oriented for funnel intaking, the elevator is lowered
+            case self.Goal.FUNNEL_INTAKE:
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.FUNNEL_INTAKE)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.DEFAULT)
+
+            # goal for ground intaking: the pivot is oriented for ground intake, the elevator is lowered
+            case self.Goal.GROUND_INTAKE:
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.GROUND_INTAKE)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.DEFAULT)
+
+            # goal for scoring in the net: the pivot is oriented for scoring in the net, the elevator is raised to net level
+            case self.Goal.ALGAE_SCORING_NET:
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.NET_SCORING)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.NET)
+
+            # goal for scoring algae in the processor: pivot oriented for processor, elevator is lowered
+            case self.Goal.ALGAE_SCORING_PROCESSOR:
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.PROCESSOR_SCORING)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.DEFAULT)
+
+            # goals for intaking algae from L2 and L3
+            case self.Goal.L2_ALGAE_INTAKE:
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.ALGAE_INTAKE)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.L2_ALGAE)
+
+            case self.Goal.L3_ALGAE_INTAKE:
+                self.pivot.set_desired_state(PivotSubsystem.SubsystemState.ALGAE_INTAKE)
+                self.elevator.set_desired_state(ElevatorSubsystem.SubsystemState.L3_ALGAE)
+
+
     def _set_goal(self, goal: Goal) -> None:
+        # if the goal is already set to this goal, return, otherwise set our goal
         if goal is self._goal:
             return
         self._goal = goal
-        
+
+
     def set_goal_command(self, goal: Goal) -> Command:
+        """
+        Return a command that sets the superstructure goal to whatever the desired goal is.
+
+        :param goal: The desired goal
+        :type goal:  Goal
+        :return:     A command that will set the desired goal
+        :rtype:      Command
+        """
         return cmd.startEnd(
             lambda: self._set_goal(goal),
             lambda: self._set_goal(self.Goal.DEFAULT),
