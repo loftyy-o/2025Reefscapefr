98abe828a70c4361d5354c8dd6126660b267faec
ManicManiac <74989150+ManicManiac64@users.noreply.github.com>
2025-02-08 14:29:04 -0800

    Fix constants.py to old version

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
+from phoenix6.signals import GravityTypeValue
 from robotpy_apriltag import AprilTagField, AprilTagFieldLayout
 from phoenix6.configs.config_groups import Slot0Configs
 from wpimath import units
@@ -6,7 +7,7 @@ apriltag_layout = AprilTagFieldLayout.loadField(AprilTagField.k2025Reefscape)
 
 class Constants:
 
-    class MotorIDs():
+    class MotorIDs:
 
         LEFT_LIFT_MOTOR = 10
         RIGHT_LIFT_MOTOR = 11
@@ -25,13 +26,28 @@ class Constants:
             .with_k_v(0.0)
             .with_k_a(0.0)
         )
-
-        L1_SCORE_POSITION = 0 # Placeholders
-        L2_SCORE_POSITION = 0
-        L3_SCORE_POSITION = 0
-        L4_SCORE_POSITION = 0
-
+    
+    class ElevatorConstants:
+        L1_SCORE_POSITION = 1 # Placeholders
+        L2_SCORE_POSITION = 2
+        L3_SCORE_POSITION = 3
+        L4_SCORE_POSITION = 4
+        L2_ALGAE_POSITION = 2.5
+        L3_ALGAE_POSITION = 3.5
+        NET_SCORE_POSITION = 5
         DEFAULT_POSITION = 0
+
+        GEAR_RATIO = 31/4 # Placeholder
+        GAINS = (Slot0Configs()
+            .with_k_g(0.03)
+            .with_k_p(1.0)
+            .with_k_i(0.0)
+            .with_k_d(0.0)
+            .with_k_s(0.0)
+            .with_k_v(0.0)
+            .with_k_a(0.0)
+            .with_gravity_type(GravityTypeValue.ELEVATOR_STATIC)
+        )
 
     class PivotConstants:
 
@@ -59,5 +75,15 @@ class Constants:
 
     class IntakeConstants:
 
-        INTAKE_SPEED = (lambda x, y: (x*63%y))(int(0o123), 4.1)
-        OUTPUT_SPEED = 0
+        INTAKE_SPEED = 1
+        OUTPUT_SPEED = 1
+
+        GEAR_RATIO = 4
+        GAINS = (Slot0Configs()
+            .with_k_p(1.0)
+            .with_k_i(0.0)
+            .with_k_d(0.0)
+            .with_k_s(0.0)
+            .with_k_v(0.0)
+            .with_k_a(0.0)
+        )
