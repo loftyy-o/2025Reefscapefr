36cc855ce13ed7fb21573ffba1c5f5edf3b13abb
loftyy-o <chimao.okafor123@gmail.com>
2025-03-27 17:12:47 -0700

    Update superstructure.py

--- subsystems/superstructure.py
+++ subsystems/superstructure.py
@@ -108,7 +108,7 @@ class Superstructure(Subsystem):
             self.pivot.set_desired_state(self._desired_pivot_state)
 
         # If climber motor position is at the top position (1 is the placeholder for what the value would actually be), it will go to the full climb state
-        if self.climber.get_position() > 1 and self.climber.get_current_state() is ClimberSubsystem.SubsystemState.CLIMB_IN:
+        if self.climber.get_position() > 0.45 and self.climber.get_current_state() is ClimberSubsystem.SubsystemState.CLIMB_IN:
             self.climber.set_desired_state(ClimberSubsystem.SubsystemState.CLIMB_IN_FULL)
 
     def simulationPeriodic(self) -> None:
